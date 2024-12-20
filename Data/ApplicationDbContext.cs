9e05f72d43c05b91cc94f23e12128373a338490d
husada20 <husadahutasoit@gmail.com>
2024-12-20 13:22:58 +0700

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeAdminPortal_CRUD.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal_CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
